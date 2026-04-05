937e8a971dd9b26290e8715a90291a487c3312d0
TiaSinghania <samanvita.singhania@gmail.com>
2026-04-05 11:20:02 -0400

    finished pwm math

--- mobotpi/constants.py
+++ mobotpi/constants.py
@@ -1,5 +1,5 @@
-#all measurements are in meters
-WHEEL_R = 0.0425 
+WHEEL_R = 0.0425 # m
 #center of wheel to center of wheel
-WHEELBASE =  0.245
-SPEED = 1
+WHEELBASE =  0.245 # m
+CHASSIS_SPEED = 1 # m/s
+MAX_WHEEL_RPM = 6500 # rpm --> calculated based on motors going 650 RPM/Volt at 22 Volts split across 2 wheels
--- mobotpi/drivetrain.py
+++ mobotpi/drivetrain.py
@@ -1,8 +1,17 @@
 from mobotpi.constants import *
 from rpi_hardware_pwm import HardwarePWM
+import math
 # omega: desired angular rate of change of the entire system
 def drive(omega) :
-    left_rot_speed = SPEED/WHEEL_R - (WHEELBASE * omega)/(2 * WHEEL_R)
-    right_rot_speed = SPEED/WHEEL_R + (WHEELBASE * omega)/(2 * WHEEL_R)
+    left_rot_speed = CHASSIS_SPEED/WHEEL_R - (WHEELBASE * omega)/(2 * WHEEL_R) # radians/ second
+    right_rot_speed = CHASSIS_SPEED/WHEEL_R + (WHEELBASE * omega)/(2 * WHEEL_R) # radians / second
+
+    left_duty_cycle = (left_rot_speed * 1/(math.pi * 2) * 60) / MAX_WHEEL_RPM # percentage
+    right_duty_cycle = (right_rot_speed * 1/(math.pi * 2) * 60) / MAX_WHEEL_RPM # percentage
+
+    left_pwm.change_duty_cycle(left_duty_cycle)
+    right_pwm.change_duty_cycle(right_duty_cycle)
+
+
     
     
